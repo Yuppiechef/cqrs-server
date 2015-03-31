10a9fa63f185fa005be923cf1214e692039ee3f2
Deon Moolman <deon@yuppiechef.com>
2015-03-31 23:33:08 +0200

    Update to clojure 1.7.0-alpha6

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies
-  [[org.clojure/clojure "1.7.0-alpha5"]
+  [[org.clojure/clojure "1.7.0-alpha6"]
    [org.clojure/tools.logging "0.3.1"]
    [org.clojure/java.data "0.1.1"]
    [prismatic/schema "0.4.0"]
